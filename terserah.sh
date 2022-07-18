b6431535ae64dde710915212893a5e04b26a8204
Terserahade <109490428+Terserahade@users.noreply.github.com>
2022-07-18 07:43:15 +0700

    Add files via upload

---
+++ terserah.sh
@@ -0,0 +1,97 @@
+clear
+figlet tools ade | lolcat
+echo
+  echo
+   echo "#####################################################" | lolcat
+   echo "[+] Nickname    : adetriwibowo   [+]" | lolcat
+   echo "[+] team        : tidak ada team [+]" | lolcat
+   echo "[+] scrip       : program tools  [+]" | lolcat
+   echo "#####################################################" | lolcat
+echo
+  echo
+   echo "#####################################################" | lolcat
+   echo "[1] UPDATE SCRIP TOOLS"  | lolcat
+   echo "[2] EXIT-KLUAR TOOLS"    | lolcat
+   echo "[3] SPAM-BIASA-V1"       | lolcat
+   echo "[4] SPAM-24-Hzjajahsjssuskrebammarexecuted1716bajabs" | lolcat
+   echo "[5] Juzz,ama dan bacaan yang lain" | lolcat 
+   echo "#####################################################" | lolcat
+   echo
+  echo
+echo
+read -p "===========> : " don
+if [ $don = 1 ]
+then
+clear
+echo
+     echo
+     echo
+apt update && apt upgrade -y
+apt install git -y
+apt install python -y
+pkg install python2 -y
+apt install toilet -y
+apt install ruby -y
+pip install requests -y
+pip install mechanize -y
+pkg install bash -y
+pkg install nano -y
+login
+echo
+fi
+
+if [ $don = 2 ]
+then
+clear
+
+    echo
+    echo
+logout
+login
+fi
+
+if [ $don = 3 ]
+then
+clear
+
+    echo
+    echo
+pkg update && pkg upgrade -y
+pkg install git -y
+pkg install python -y
+pip install requests -y
+pip install bs4
+pip install colorama
+pip install mechanize
+git clone https://github.com/DANZZ-XD/spam-v1
+cd spam-v1
+python spam-v1.py
+fi
+
+if [ $don = 4 ]
+then
+clear
+
+    echo
+    echo
+pkg update && pkg upgrade -y
+pkg install python git bash -y
+pkg install python2
+git clone https://github.com/AmmarrBN/Tools-WhatsApp
+cd Tools-WhatsApp
+bash install.sh
+fi
+
+if [ $don = 5 ]
+then
+clear
+
+    echo
+    echo
+pkg update && pkg upgrade -y
+pkg install git -y
+git clone https://github.com/MrVirusSpm-07/juz-amma
+cd juz-amma
+sh amma.sh
+fi
+
